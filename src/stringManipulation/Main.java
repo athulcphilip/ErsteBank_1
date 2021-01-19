32469d6d020744aef26bf750d34c9e8a63e0383d
athulcphilip <athulcphilip@gmail.com>
2021-01-19 18:44:13 +0530

    Iteration 2 : Read Input From Console and Display Output on Console

--- src/stringManipulation/Main.java
+++ src/stringManipulation/Main.java
@@ -1,4 +1,27 @@
 package stringManipulation;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
+
+
+    public static void main(String args[]){
+
+        System.out.println("Enter Text :");
+
+        try {
+            final BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(System.in));
+
+            final String text = reader.readLine();
+
+            System.out.println("Number of Words : " + Words.wordCount(text));
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
--- src/stringManipulation/Words.java
+++ src/stringManipulation/Words.java
@@ -12,16 +12,13 @@ public class Words {
         if (temp != null && temp.length() > 0) {
             for (int i = 0; i < temp.length() - 1; i++) {
 
-                //if (isChar(temp.charAt(i)) && !(isChar(temp.charAt(i-1)))) {
                 if (isChar(temp.charAt(i))) {
                     if ((i - 1 >= 0 && !isChar(temp.charAt(i - 1)) || (i - 1 < 0))) {
                         count++;
                     }
                 }
-
             }
         }
-        //count ++;
 
         return count;
 
